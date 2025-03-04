b7caf5bb0532e0d86556f3af7c1e8080c72bce9f
stephenChinag <stephenchinag@gmail.com>
2025-03-04 12:24:26 +0100

    library

--- MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
+++ MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fd9e4ffbbd082ccfa810fff4642757641b7a355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327a4f763278af9f471673a0f08487564f18d0ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
